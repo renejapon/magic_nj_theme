0596ea86c058f1ea96ecfee0b4b974cad217501d
ReneJapon <renejapon@gmail.com>
2017-12-26 19:58:17 +0100

    modificaciones content front-page.php

--- wp-content/themes/NewTheme/front-page.php
+++ wp-content/themes/NewTheme/front-page.php
@@ -1,9 +1,13 @@
 <?php get_header(); ?>
 	<div class="container">
 		<?php get_sidebar(); ?>
-		<section class="row">
+		<section class="content">
 			<div class="row">
 				<h1>"fron-page.php"</h1>
+				<article class="">
+					<?php $descripcion = get_bloginfo('description','display'); ?>
+					<h1 class="site-title"><?php echo $descripcion ?></h1>
+				</article>
 			</div>
 		</section>
 
--- wp-content/themes/NewTheme/index.php
+++ wp-content/themes/NewTheme/index.php
@@ -2,10 +2,7 @@
 <div class="container">
 	<?php  get_sidebar()?>
 	<section class="row">
-		<article class="">
-			<?php $descripcion = get_bloginfo('description','display'); ?>
-			<h1 class="site-title"><?php echo $descripcion ?></h1>
-		</article>
+		
 	</section>
 </div>
 <?php get_footer(); ?>
